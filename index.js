bba9c6d29c2ad801b2dd290857a9c42745023827
Hora b <horaciob@gmail.com>
2016-08-20 15:39:56 -0300

    delete express file

--- index.js
+++
@@ -1,8 +0,0 @@
-require('newrelic')
-var express = require('express');
-var app = express();
-
-var server = app.listen(8080, function () {
-    var port = server.address().port;
-    console.log('Example app listening at http://localhost:%s', port);
-});
